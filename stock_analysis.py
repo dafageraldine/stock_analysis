e142cedf4d822bf76efe02c643bce9248cc89e3f
geraldineDafa <dafageraldine16@gmail.com>
2023-05-17 22:52:40 +0700

    done added knn for choosing best algorithm

--- stock_analysis.py
+++ stock_analysis.py
@@ -1,7 +1,8 @@
 import pandas as pd
+import math
 
 # Read the stock data into a Pandas DataFrame
-df = pd.read_csv('AAPL_data.csv')  # Replace 'stock_data.csv' with the path to your data file
+df = pd.read_csv('MSFT_data.csv')
 
 # Convert the date column to a datetime data type
 df['date'] = pd.to_datetime(df['date'])
@@ -15,107 +16,83 @@ df['MA10'] = df['close'].rolling(window=10).mean()
 df['MA20'] = df['close'].rolling(window=20).mean()
 df['MA30'] = df['close'].rolling(window=30).mean()
 df['MA50'] = df['close'].rolling(window=50).mean()
+df['MA100'] = df['close'].rolling(window=100).mean()
+df['MA200'] = df['close'].rolling(window=200).mean()
 
-ma = 'MA'
-lowerma = ma + '10'
-higherma =  ma + '50'
+listdata = []
+listma = ['5','10','20','30','50','100','200']
+for x in range(len(listma)):
+    ma = 'MA'
+    for y in range(len(listma)):
+        lowerma = ma + listma[x]
+        if (x == y or x > y):
+            pass
+        else:
+            higherma =  ma + listma[y]
 
-# Print the DataFrame with moving averages
-# print(df)
+            # Initialize the 'Signal' column with 'Hold' for no action
+            df['Signal'] = 'Hold'
 
-# df['Crossing'] = None
-# for i in range(1, len(df)):
-#     if df['MA5'].iloc[i] > df['MA10'].iloc[i] and df['MA5'].iloc[i - 1] < df['MA10'].iloc[i - 1]:
-#         df.loc[i, 'Crossing'] = 'MA5 crossing above MA10'
-#     elif df['MA5'].iloc[i] < df['MA10'].iloc[i] and df['MA5'].iloc[i - 1] > df['MA10'].iloc[i - 1]:
-#         df.loc[i, 'Crossing'] = 'MA5 crossing below MA10'
+            # Identify buy signals (lowerMA crossing above HigherMA)
+            buy_indices = df[(df[lowerma] > df[higherma]) & (df[lowerma].shift() < df[higherma].shift())].index
+            df.loc[buy_indices, 'Signal'] = 'Buy'
 
-# Print the DataFrame with MA crossings
-# print(df[['date', 'close', 'MA5', 'MA10', 'Crossing']])
+            # Identify sell signals (lowerMA crossing below HigherMA)
+            sell_indices = df[(df[lowerma] < df[higherma]) & (df[lowerma].shift() > df[higherma].shift())].index
+            df.loc[sell_indices, 'Signal'] = 'Sell'
 
-# crossings = []
-# for i in range(1, len(df)):
-#     if df['MA5'].iloc[i] > df['MA10'].iloc[i] and df['MA5'].iloc[i - 1] < df['MA10'].iloc[i - 1]:
-#         crossings.append((df['date'].iloc[i], 'MA5 crossing above MA10'))
-#     elif df['MA5'].iloc[i] < df['MA10'].iloc[i] and df['MA5'].iloc[i - 1] > df['MA10'].iloc[i - 1]:
-#         crossings.append((df['date'].iloc[i], 'MA5 crossing below MA10'))
+            # Filter and print only the rows with buy and sell signals
+            filtered_df = df[df['Signal'].isin(['Buy', 'Sell'])]
 
-# # Print the crossing data
-# for crossing in crossings:
-#     print(crossing)
+            # Calculate estimated returns
+            previous_buy_index = None
+            estimated_returns = []
+            for index, row in df.iterrows():
+                
+                if row['Signal'] == 'Buy':
+                    previous_buy_index = index
+                    # if((str(lowerma)+"&"+str(higherma) ) == 'MA20&MA100'):
+                    #     print("buy action " + str(row["date"]) + " price close " + str(row['close']))
+                elif row['Signal'] == 'Sell' and previous_buy_index is not None:
+                    # if((str(lowerma)+"&"+str(higherma) ) == 'MA20&MA100'):
+                    #     print("sell action " + str(row["date"] )+ " price close " + str(row['close']))
+                    buy_close = df.loc[previous_buy_index, 'close']
+                    sell_close = row['close']
+                    estimated_return = ((sell_close - buy_close) / buy_close) * 100
+                    estimated_returns.append(estimated_return)
 
-# Initialize the 'Signal' column with 'Hold' for no action
-# df['Signal'] = 'Hold'
+            returnall = 0
+            lost = 0
+            profit = 0
+            biggest_lost = 0
+            biggest_profit = 0
+            for i, return_value in enumerate(estimated_returns, start=1):
+                returnall = returnall + return_value
+                if( (biggest_profit == 0 or biggest_profit < return_value) and return_value > 0):
+                    biggest_profit = return_value
+                if( (biggest_lost == 0 or (biggest_lost * -1) < (return_value * -1) ) and return_value < 0):
+                    biggest_lost = return_value
+                # if((str(lowerma)+"&"+str(higherma) ) == 'MA20&MA100'):
+                #     print("trade " + str(i) + " " + str(return_value))
+                if(return_value < 0):
+                    lost = lost + 1
+                elif(return_value > 0):
+                    profit = profit + 1
+            if(lost != 0 and profit != 0):
+                listdata.append({"ma_compared": str(lowerma)+"&"+str(higherma),"probability": profit/(profit+lost) * 100,"estimated_return": returnall,"lost" : lost, "profit": profit, "biggest_profit" : biggest_profit, "biggest_lost": biggest_lost})
+            
+arrange_bydis = []
+for k in range(len(listdata)):
+    data = listdata[k]
+    dis = math.sqrt(pow((100 - data["probability"]),2)+  pow((0 - data["biggest_lost"]),2))
+    arrange_bydis.append({"ma_compared": listdata[k]["ma_compared"], "dis" : dis})
 
-# # Identify buy signals (MA5 crossing above MA10)
-# buy_indices = df[(df['MA5'] > df['MA10']) & (df['MA5'].shift(1) < df['MA10'].shift(1))].index
-# df.loc[buy_indices, 'Signal'] = 'Buy'
+sorted_array = sorted(arrange_bydis, key=lambda x: x["dis"])
 
-# # Identify sell signals (MA5 crossing below MA10)
-# sell_indices = df[(df['MA5'] < df['MA10']) & (df['MA5'].shift(1) > df['MA10'].shift(1))].index
-# df.loc[sell_indices, 'Signal'] = 'Sell'
+for i in range(len(sorted_array)):
+    for j in range(len(listdata)):
+        if(sorted_array[i]["ma_compared"] == listdata[j]["ma_compared"]):
+            print("---------------------------------")
+            print(listdata[j])
+            print("---------------------------------")
 
-# # Print the DataFrame with signals
-# print(df[['date', 'close', 'MA5', 'MA10', 'Signal']])
-
-# Initialize the 'Signal' column with 'Hold' for no action
-df['Signal'] = 'Hold'
-
-# Identify buy signals (MA5 crossing above MA10)
-buy_indices = df[(df[lowerma] > df[higherma]) & (df[lowerma].shift(1) < df[higherma].shift(1))].index
-df.loc[buy_indices, 'Signal'] = 'Buy'
-
-# Identify sell signals (MA5 crossing below MA10)
-sell_indices = df[(df[lowerma] < df[higherma]) & (df[lowerma].shift(1) > df[higherma].shift(1))].index
-df.loc[sell_indices, 'Signal'] = 'Sell'
-
-# Filter and print only the rows with buy and sell signals
-filtered_df = df[df['Signal'].isin(['Buy', 'Sell'])]
-# print(filtered_df[['date', 'close', 'MA20', 'MA50', 'Signal']])
-
-# Initialize the 'Signal' column with 'Hold' for no action
-# df['Signal'] = 'Hold'
-
-# # Identify buy signals (MA5 crossing above MA10)
-# buy_indices = df[(df['MA5'] > df['MA10']) & (df['MA5'].shift(1) < df['MA10'].shift(1))].index
-# df.loc[buy_indices, 'Signal'] = 'Buy'
-
-# # Identify sell signals (MA5 crossing below MA10)
-# sell_indices = df[(df['MA5'] < df['MA10']) & (df['MA5'].shift(1) > df['MA10'].shift(1))].index
-# df.loc[sell_indices, 'Signal'] = 'Sell'
-
-# # Filter the data within the specified date range
-# start_date = pd.to_datetime('2018-01-08 00:00:00')
-# end_date = pd.to_datetime('2018-01-25 00:00:00')
-# filtered_df = df[(df['date'] >= start_date) & (df['date'] <= end_date)]
-
-# # Print the filtered data
-# print(filtered_df[['date', 'close', 'MA5', 'MA10', 'Signal']])
-
-# Calculate estimated returns
-previous_buy_index = None
-estimated_returns = []
-for index, row in df.iterrows():
-    if row['Signal'] == 'Buy':
-        previous_buy_index = index
-    elif row['Signal'] == 'Sell' and previous_buy_index is not None:
-        buy_close = df.loc[previous_buy_index, 'close']
-        sell_close = row['close']
-        estimated_return = (sell_close - buy_close) / buy_close
-        estimated_returns.append(estimated_return)
-
-# Print the estimated returns
-# print("Estimated Returns:")
-returnall = 0
-lost = 0
-profit = 0
-for i, return_value in enumerate(estimated_returns, start=1):
-    # print(f"Trade {i}: {return_value:.2%}")
-    returnall = returnall + return_value
-    if(return_value < 0):
-        lost = lost + 1
-    elif(return_value > 0):
-        profit = profit + 1
-
-print("all return calculated : " + str(returnall))
-print("lost : " + str(lost) + " profit : " + str(profit) + " prosentase " + str(profit/(profit+lost) * 100))
